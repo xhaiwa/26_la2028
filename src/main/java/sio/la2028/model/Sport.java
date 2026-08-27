bee5ea49e1226d03a5872bdbe4b1c5e338ea5335
xhaiwa <clement.jouenne.pro@gmail.com>
2026-08-27 16:19:40 +0200

    feat: Model sport

---
+++ src/main/java/sio/la2028/model/Sport.java
@@ -0,0 +1,27 @@
+package sio.la2028.model;
+
+public class Sport {
+    private int id;
+    private String nom;
+
+    public Sport(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
